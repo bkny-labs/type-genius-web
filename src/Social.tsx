43cab1ca74d09817a12cdfc0d245cd5fe3a840a1
Michael Fortuna <m4tuna@gmail.com>
2023-04-12 14:57:15 -0400

    updated socials

--- src/Social.tsx
+++ src/Social.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { FaEnvelope, FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaChrome, FaEnvelope, FaGithub, FaLinkedin } from 'react-icons/fa';
 
 export default function Social() {
     return (
@@ -9,11 +9,14 @@ export default function Social() {
                     <a href="https://github.com/bkny-labs/type-genius" rel="noreferrer" target="_blank"><FaGithub /></a>
                 </div>
                 <div className="column shrink">
+                    <a href="https://chrome.google.com/webstore/" rel="noreferrer" target="_blank"><FaChrome /></a>
+                </div>
+                {/* <div className="column shrink">
                     <a href="#" rel="noreferrer" target="_blank"><FaLinkedin /></a>
                 </div>
                 <div className="column shrink">
                     <a href="mailto:m4tuna@gmail.com" rel="noreferrer" target="_blank"><FaEnvelope /></a>
-                </div>
+                </div> */}
             </div>
         </div>
     );
